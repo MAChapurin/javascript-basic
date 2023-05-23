0843ae5e4a779f720ae218ec484840b4c78c8374
Michael <machapurin@icloud.com>
2023-05-23 13:06:05 +0300

    driver check is finished

---
+++ 06_driver/index.js
@@ -0,0 +1,9 @@
+const hasLicence = true;
+const age = 18;
+const isDrunk = false;
+
+if(hasLicence && age >=18 && !isDrunk) {
+  console.log('Может сесть за руль')
+} else {
+  console.log('Не может сесть за руль')
+}
