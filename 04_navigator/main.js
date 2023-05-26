db6a350679afeddeb1387662ffe69d99fa0a860a
Michael <machapurin@icloud.com>
2023-05-26 14:29:05 +0300

    added missin symbols in code

--- 04_navigator/main.js
+++ 04_navigator/main.js
@@ -10,6 +10,6 @@ form.addEventListener('submit', (e)=> {
   console.log(addressLat.value, addressLong.value, positionLat.value, positionLong.value)
   const catetOne = Math.abs(Number(addressLat.value) - Number(positionLat.value));
   const catetTwo = Math.abs(Number(addressLong.value) - Number(positionLong.value));
-  const result = Math.sqrt(Math.pow(catetOne, 2) + Math.pow(catetTwo, 2))
+  const result = Math.sqrt(Math.pow(catetOne, 2) + Math.pow(catetTwo, 2));
   document.getElementById('result').textContent = result.toFixed(2);
 })
--- 06_driver/index.js
+++ 06_driver/index.js
@@ -2,7 +2,7 @@ const hasLicence = true;
 const age = 18;
 const isDrunk = false;
 
-if(hasLicence && age >=18 && !isDrunk) {
+if (hasLicence && age >=18 && !isDrunk) {
   console.log('Может сесть за руль')
 } else {
   console.log('Не может сесть за руль')
