b0f8b539b969a4d33d622c7b45bf7d317bd5ca90
Michael <machapurin@icloud.com>
2023-05-31 21:01:13 +0300

    added fn last day of month

--- 11_date_array/index.js
+++ 11_date_array/index.js
@@ -1,32 +1,33 @@
 function isDate(arr) {
   let regexp = /[\-\(\)\.\/,]/g;
+  function getLastDayOfMonth(year, month) {
+    let date = new Date(year, month, 0);
+    return date.getDate();
+  }
   return arr
     .map((el) => {
       const divider = el.match(regexp);
       if (divider) {
         const [day, month, year] = el.split(divider[0]);
-        // const checkDay = Number(day) <= 31 && Number(day) >= 1;
-        // const checkMonth = Number(month) <= 12 && Number(month) >= 1;
-        const checkDayOrMonth =
-          (Number(day) <= 31 &&
-            Number(day) >= 1 &&
-            Number(month) <= 12 &&
-            Number(month) >= 1) ||
-          (Number(day) <= 12 &&
-            Number(day) >= 1 &&
-            Number(month) <= 31 &&
-            Number(month) >= 1);
+        const checkDay = Number(day) <= getLastDayOfMonth(year, month) && Number(day) >= 1;
+        const checkMonth = Number(month) <= 12 && Number(month) >= 1;
         const checkYear =
           year.length === 4 && typeof (Number(year) === 'number');
         //Представим что валидные только четырехзначные числа
-        return checkDayOrMonth && checkYear ? el : null;
+        return checkDay && checkMonth && checkYear ? el : null;
       }
     })
     .filter((el) => el);
 }
 
 const data = [
+  '28/02/2023',
+  '29/02/2023',
+  '29/02/2024',//Високосный год
+  '29/02/1123',
+  '29/02/1124',//Високосный год
   '32-01-2022',
+  '30-02-2022',
   '10-01-2022',
   'random string',
   '10.12.2023',
@@ -40,3 +41,8 @@ const data = [
 console.log('Начальный массив:', data);
 console.log('\n');
 console.log('Отсортированный массив: ', isDate(data));
+
+// const d = "01/30/2024";
+// const [dd,mm,yy] = d.split('/')
+// console.log(new Date(yy, mm, dd))
+
